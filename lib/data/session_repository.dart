1e30a8732d73051561595b00a34f316c1efb3a7f
Rustam <me@rus.com>
2025-08-07 09:41:59 +0300

    add random clients

--- lib/data/session_repository.dart
+++ lib/data/session_repository.dart
@@ -1,27 +1,41 @@
+import 'dart:math';
+
 import '../models/session.dart';
 
 class SessionRepository {
+  final _random = Random();
+
   Future<List<Session>> fetch() async {
-    await Future.delayed(const Duration(seconds: 1));
-    return [
-      Session(DateTime(2025, 7, 1), 'Customer A', 100),
-      Session(DateTime(2025, 7, 1), 'Customer B', 200),
-      Session(DateTime(2025, 7, 1), 'Customer C', 300),
-      Session(DateTime(2025, 7, 2), 'Customer D', 400),
-      Session(DateTime(2025, 7, 2), 'Customer E', 500),
-      Session(DateTime(2025, 7, 2), 'Customer F', 600),
-      Session(DateTime(2025, 7, 2), 'Customer A', 700),
-      Session(DateTime(2025, 7, 3), 'Customer B', 800),
-      Session(DateTime(2025, 7, 4), 'Customer C', 150),
-      Session(DateTime(2025, 7, 4), 'Customer D', 250),
-      Session(DateTime(2025, 7, 5), 'Customer E', 350),
-      Session(DateTime(2025, 7, 5), 'Customer F', 450),
-      Session(DateTime(2025, 7, 6), 'Customer G', 550),
-      Session(DateTime(2025, 7, 6), 'Customer H', 650),
-      Session(DateTime(2025, 7, 7), 'Customer I', 750),
-      Session(DateTime(2025, 7, 7), 'Customer J', 850),
-      Session(DateTime(2025, 7, 8), 'Customer K', 950),
-      Session(DateTime(2025, 7, 8), 'Customer L', 1050),
-    ];
+    final int totalDates = _random.nextInt(365) + 1;
+    final int totalClients = _random.nextInt(1000) + 1;
+
+    final now = DateTime.now();
+    final dates = List.generate(totalDates, (i) {
+      final d = now.subtract(Duration(days: i));
+      return DateTime(d.year, d.month, d.day);
+    });
+
+    final sessions = <Session>[];
+
+    final clientsPerDate = (totalClients / totalDates).ceil();
+    int clientCounter = 0;
+
+    for (final date in dates) {
+      for (int i = 0; i < clientsPerDate; i++) {
+        if (clientCounter >= totalClients) break;
+
+        sessions.add(
+          Session(
+            date,
+            'Клиент ${clientCounter + 1}',
+            _random.nextInt(1000) + 1,
+          ),
+        );
+
+        clientCounter++;
+      }
+    }
+
+    return sessions;
   }
 }
